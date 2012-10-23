1c9cb9f47fb95b21aed37a5d3cb94efca7a530c4
Gummi <gummi@hugveitan.is>
2012-10-23 15:52:24 +0000

    Fixed jsessionid rewrite bug.

--- src/java/com/idega/servlet/filter/IWUrlRewriteFilter.java
+++ src/java/com/idega/servlet/filter/IWUrlRewriteFilter.java
@@ -1,6 +1,8 @@
 package com.idega.servlet.filter;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -17,6 +19,12 @@ public class IWUrlRewriteFilter extends BaseFilter {
 	
 	public static String SLASH = "/";
 	
+	private static final Pattern JSESSIONID_PATTERN = Pattern.compile("(?i)^(.*);jsessionid=[\\w\\.\\-]+(.*)");
+	private static final String JSESSIONID_REPLACEMENT = "$1$2";
+	
+
+	private static final Pattern RESOURCE_PATTERN = Pattern.compile(".*\\.[\\w]+");
+	
 	public void init(FilterConfig config) throws ServletException {
 		// TODO Auto-generated method stub
 	}
@@ -29,17 +37,23 @@ public class IWUrlRewriteFilter extends BaseFilter {
 		HttpServletRequest request = (HttpServletRequest)srequest;
 		HttpServletResponse response = (HttpServletResponse)sresponse;
 		
-		String requestUri = request.getRequestURI();
-		if(requestUri != null && !requestUri.endsWith(SLASH)){
-			boolean isResourceEnding = requestUri.matches(".*\\.[\\w]+");
+		/* Remove session id from request url */
+		String requestUrl = request.getRequestURI();
+		Matcher sessionIdMatcher = JSESSIONID_PATTERN.matcher(requestUrl);
+		if (sessionIdMatcher.matches()) {
+			requestUrl = sessionIdMatcher.replaceFirst(JSESSIONID_REPLACEMENT);
+		}
+		
+		if(requestUrl != null && !requestUrl.endsWith(SLASH)){
+			boolean isResourceEnding = RESOURCE_PATTERN.matcher(requestUrl).matches();
 			if(!isResourceEnding){
 				String queryString = request.getQueryString();
 				String redirectURI;
 				if(queryString!=null){
-					redirectURI = requestUri+"/?"+queryString;
+					redirectURI = requestUrl+"/?"+queryString;
 				}
 				else{
-					redirectURI = requestUri + SLASH;
+					redirectURI = requestUrl + SLASH;
 				}
 				response.sendRedirect(redirectURI);
 				return;
